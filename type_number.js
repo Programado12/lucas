ffdbd90006fa1fe58117e77378b1e8dd8c409b7e
lucas <lucas.castro@aluno.educ.gov.br>
2023-05-05 14:30:01 -0300

    adicionado while

--- type_number.js
+++ type_number.js
@@ -1,16 +1,8 @@
-var numero=7;
+const senha=22;
+let msenha=0;
 
-//for(let c=1;c<=11-1;c++){
-    //math=numero*c;
-    //console.log(numero+'+'+c+'='+math);
+while(msenha != senha){
+    msenha++;
 
-//}
-let c=1;
-
-while(c<=11-1){
-    math = c * numero;
-    console.log(numero+' X '+c+' = '+math);
-    c++;
 }
-
-console.log('ola mundo');
+console.log(msenha);
