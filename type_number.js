eab7deb3cb8017a714c6bad2d9125ce2ab29b068
lucas <lucas.castro@aluno.educ.gov.br>
2023-05-05 13:33:32 -0300

    adicionando o typeNumber.js

---
+++ type_number.js
@@ -0,0 +1,14 @@
+var numero=7;
+
+//for(let c=1;c<=11-1;c++){
+    //math=numero*c;
+    //console.log(numero+'+'+c+'='+math);
+
+//}
+let c=1;
+
+while(c<=11-1){
+    math = c * numero;
+    console.log(numero+' X '+c+' = '+math);
+    c++;
+}
