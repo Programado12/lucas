b8ca6ab7550b1d112b33e408a235fe88e98729da
lucas <lucas.castro@aluno.educ.gov.br>
2023-05-06 14:03:18 -0300

    adicionado Fatiando array

--- function.js
+++ function.js
@@ -1,4 +1,27 @@
-function nomeCpf(nome,cpf){
-    return 'meu nome é ${nome}'
+const alunos = [
+    'João',
+    'Juliana',
+      'Ana',
+       'Caio',
+        'Lucas',
+         'Marjorie',
+          'Guilherme',
+           'Aline',
+            'Fabiana',
+             'Andre',
+              'Carlos',
+               'Paulo',
+                'Bia',
+                 'Vivian',
+                  'Isabela',
+                  'Vinícius',
+                   'Renan',
+                    'Renata',
+                     'Daisy',
+                      'Camilo'];
 
-}
+const sala1=alunos.slice(0,alunos.length/2);
+const sala2=alunos.slice(alunos.length/2);
+console.log(sala1);
+console.log(" ");
+console.log(sala2);
